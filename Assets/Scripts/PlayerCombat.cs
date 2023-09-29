cbd1d83ce7d7b223fe8367d3143ad90fa28fbf74
hatdog42 <kristian.lillesand@gmail.com>
2023-09-29 14:44:58 +0200

    attack

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,11 +1,29 @@
+using System;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-   
+    private InputManager _inputManager;
+
+    public Transform AttackPoint;
+    public float attackRange = 0.5;
+
+    private void Start()
+    {
+        _inputManager = GetComponent<InputManager>();
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        if (_inputManager.attackPressed)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
     {
         
     }
