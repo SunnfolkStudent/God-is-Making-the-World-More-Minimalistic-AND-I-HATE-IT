a2865b5da97949f17e99bcfc7493b143c99c80de
hatdog42 <kristian.lillesand@gmail.com>
2023-09-29 14:34:38 +0200

    attack in inputmaneger

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
