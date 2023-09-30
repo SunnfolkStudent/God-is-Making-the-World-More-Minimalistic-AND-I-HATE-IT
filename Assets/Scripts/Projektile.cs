87e740cc58784e600b428f2ef2cbb7ebf386fafe
hatdog42 <kristian.lillesand@gmail.com>
2023-09-30 11:21:52 +0200

    idk

--- Assets/Scripts/Projektile.cs
+++ Assets/Scripts/Projektile.cs
@@ -1,17 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Projektile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
