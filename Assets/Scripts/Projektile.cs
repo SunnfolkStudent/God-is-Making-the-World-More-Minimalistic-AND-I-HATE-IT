e07d09abbbe371e9dc9e1368a07608136600040c
hatdog42 <kristian.lillesand@gmail.com>
2023-09-30 11:55:46 +0200

    projektile

--- Assets/Scripts/Projektile.cs
+++ Assets/Scripts/Projektile.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 public class Projektile : MonoBehaviour
 {
-    public float speed;
+    public float speed = 20f;
+    public float arc = 0.5f;
+
+    public Rigidbody2D _rigidbody2D;
 
     private void Start()
     {
-        
+        _rigidbody2D.velocity = new Vector2(transform.position.x * speed,transform.position.x * arc);
     }
+    
 }
