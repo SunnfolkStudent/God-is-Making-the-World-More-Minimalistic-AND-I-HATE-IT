e5642e146de89f62ce9154ebc48c9991a8d7f2e2
1Polybius1 <146327892+1Polybius1@users.noreply.github.com>
2023-10-05 15:14:03 +0200

    Certain changes

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,8 +1,29 @@
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public static string lastSceneWeWereOn;
+    private string _previousScene;
+
+    private void Awake()
+    {
+        _previousScene = lastSceneWeWereOn;
+    }
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "DeathScene") return;
+        lastSceneWeWereOn = SceneManager.GetActiveScene().name;
+    }
+
+    public void LoadPreviousScene()
+    {
+        
+        SceneManager.LoadScene(_previousScene);
+    }
+
     public void LoadSceneByName(string name)
     {
         SceneManager.LoadScene(name);
