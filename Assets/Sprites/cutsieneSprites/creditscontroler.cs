9c192a992ccf085b5fb57ab8d65d8d41e834c0bd
hatdog42 <kristian.lillesand@gmail.com>
2023-10-05 18:22:30 +0200

    credits done

---
+++ Assets/Sprites/cutsieneSprites/creditscontroler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditscontroler : MonoBehaviour
+{
+    private float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; // Count seconds
+            
+        if (timer > 13)
+        { 
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
