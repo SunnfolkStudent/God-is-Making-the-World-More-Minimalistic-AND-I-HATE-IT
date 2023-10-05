9f70259e0d7079c11a32b1079e92379c350dd06a
1Polybius1 <146327892+1Polybius1@users.noreply.github.com>
2023-10-05 20:11:52 +0200

    HEEEEEEEEEEEEELPPP MEEEEEEE

---
+++ Assets/Scripts/God Scripts/GodLvlThreeDialog.cs
@@ -0,0 +1,184 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.Serialization;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class GodLvlThreeDialog : MonoBehaviour
+{
+    [Header("Dialogue")]
+    public GameObject dialoguePanel;
+    public GameObject GodInteractPromt;
+    public GameObject God;
+    public TextMeshProUGUI dialogueText;
+    public TextMeshProUGUI nameText;
+    public string[] dialogue; 
+    public string[] name;
+        
+    [Header("Player Input & Movement")]
+    public InputCubeManager _inputManager;
+    public PlayerRecktangelMovement _playerMovement;
+    
+    private int index = 0;
+
+    [Header("Audio")]
+    public AudioSource _audioSource;
+    public AudioClip[] godClips;
+    public AudioClip[] minimalistGodClips;
+    public AudioClip[] joeClips;
+    public AudioClip harp;
+
+    [Header("Word Speed & Disable Movement")]
+    public float wordSpeed;
+    public bool playerIsClose;
+    //public bool canGoToNextLine = true;
+
+    [Header("Animation & Camera")]
+    public PlayableDirector playableDirector;
+
+    public GameObject dialogueCamPos;
+    public Transform tFollowTarget;
+    public CinemachineVirtualCamera vcam;
+    
+    private float timer;
+    private bool timerDone;
+    private bool timerOn = false;
+
+    public PlayableDirector crushed;
+    void Start()
+    {
+        dialogueText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerOn)
+        {
+            timer += Time.deltaTime;
+            if (timer > 1.5f && !timerDone)
+            {
+                if (SceneManager.GetActiveScene().name.Equals("HeavenScene")) { SceneManager.LoadScene("LevelTwo"); }
+                if (SceneManager.GetActiveScene().name.Equals("HeavenSceneTwo")) { SceneManager.LoadScene("LevelThree"); }
+                if (SceneManager.GetActiveScene().name.Equals("HeavenSceneThree")) { SceneManager.LoadScene("LevelFour"); }
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            
+            
+            tFollowTarget = dialogueCamPos.transform; 
+            vcam.LookAt = tFollowTarget; 
+            vcam.Follow = tFollowTarget;
+            vcam.m_Lens.OrthographicSize = 5f;
+            
+            if (!dialoguePanel.activeInHierarchy)
+            {
+                if (index == 0)
+                {
+                    God.SetActive(true);
+                    _audioSource.PlayOneShot(harp);
+                    playableDirector.Play(); 
+                }
+                
+                _inputManager.canMove = false; 
+                _playerMovement.canMove = false; 
+                dialoguePanel.SetActive(true); 
+                GodInteractPromt.SetActive(false);
+                
+                dialogueText.text = ""; 
+                nameText.text = name[index]; 
+                StartCoroutine(Typing());
+                
+            }
+            else if (dialogueText.text == dialogue[index])
+            {
+                NextLine();
+            } 
+            
+            if (dialogueText.text == dialogue[index])
+            {
+                //canGoToNextLine = true;
+            }
+        }
+        
+        
+        /*if (Input.GetKeyDown(KeyCode.Q) && dialoguePanel.activeInHierarchy)
+        {
+            RemoveText();
+        }*/
+    }
+    
+    public void RemoveText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+        _inputManager.canMove = true;
+        _playerMovement.canMove = true;
+        crushed.Play();
+        timerOn = true;
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            if (letter.ToString() != " ") { PlaySpeechClip();}
+
+            if (char.IsPunctuation(letter)) { yield return new WaitForSeconds(wordSpeed * 6); }
+            else { yield return new WaitForSeconds(wordSpeed); }
+        }
+    }
+
+    private void PlaySpeechClip()
+    {
+        if (nameText.text == "God") { _audioSource.PlayOneShot(godClips[Random.Range(0, godClips.Length)]); }
+        if (nameText.text == "Minimalist God") { _audioSource.PlayOneShot(minimalistGodClips[Random.Range(0, minimalistGodClips.Length)]); }
+        else if (nameText.text == "Maximalist Joe") { _audioSource.PlayOneShot(joeClips[Random.Range(0, joeClips.Length)]); }
+        return;
+    }
+    
+    public void NextLine()
+    {
+        //canGoToNextLine = false;
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            nameText.text = name[index];
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            RemoveText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+            GodInteractPromt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            //RemoveText();
+            GodInteractPromt.SetActive(false);
+        }
+    }
+}
