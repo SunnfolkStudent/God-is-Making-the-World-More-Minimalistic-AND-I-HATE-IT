67cd0bbac5e0ccddf0021bbf0d477fca63fcf409
hatdog42 <kristian.lillesand@gmail.com>
2023-10-20 19:20:51 +0200

    apple particles

--- Assets/Scripts/Projektile.cs
+++ Assets/Scripts/Projektile.cs
@@ -1,8 +1,11 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Projektile : MonoBehaviour
 {
+    public ParticleSystem particles;
+    
     public float speed = 20f;
     public float arc = 0.5f;
 
@@ -10,10 +13,17 @@ public class Projektile : MonoBehaviour
 
     public float shootDirection;
 
+    private float timer;
+    private bool timerDone;
+    private bool timerOn = false;
     private void Start()
     {
         
         rigidbody2D.velocity = new Vector2(shootDirection * speed,arc);
     }
-    
+
+    private void OnDestroy()
+    {
+        Instantiate(particles, transform.position, quaternion.identity);
+    }
 }
